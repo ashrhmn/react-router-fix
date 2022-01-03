10faf58d9c8f7b5090bfe04f670855035c1c2667
Ashik Rahman <ashrhmn99@gmail.com>
2022-01-03 13:13:29 +0600

    Add unknown route page

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ const App = () => {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/profile">Profile</Link>
+        <Link to="/qwertyuiop">Unknown Route</Link>
       </ul>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/profile" element={<h1>Profile</h1>} />
+        <Route path="/*" element={<h1>404 not found</h1>} />
       </Routes>
     </div>
   )
